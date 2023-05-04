144757a9435011fd220f09fccd6af6643278e83b
AndriySachyk <asachyk1312@gmail.com>
2023-05-05 00:37:12 +0300

    update

---
+++ src/js/01-gallery.js
@@ -0,0 +1,30 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+const listGalleryUl = document.querySelector('.gallery');
+const itemsGallery = createGalleryItem(galleryItems);
+
+listGalleryUl.insertAdjacentHTML('beforeend', itemsGallery);
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`;
+    })
+    .join('');
+}
+
+let sliderGallery = new SimpleLightbox('.gallery li a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+
+listGalleryUl.style.listStyle = "none"
